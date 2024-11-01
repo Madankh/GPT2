bf2eabbb2e49d339f283c1bf759615449f46cf9e
Madankh <khadkasuman0000@gmail.com>
2024-11-01 14:25:43 +0545

    new

--- repeatGPT2.py
+++ repeatGPT2.py
@@ -87,6 +87,85 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+    
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingfacve"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        # assert model_type in {'gpt-2', 'gpt2_medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
 
+        # n_;ater. n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12,n_head = 12, n_embd = 768), # 124M parameters
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model
+        config_args['block_size'] = 1024
         
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer not param
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
+    
+    def forward(self, idx, targets=None):
+        B,T = idx.size()
+
+        assert T<=self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # Forward the token and position embding  
+        pos = torch.arange(0,T,dtype=torch.long, device=idx.device)
+        pos_emd = self.tranformer.wpe(pos)
+        tok_emd = self.tranformer.wte(idx)
+        x = pos_emd + tok_emd
+        # forward the blocks of the transformer
+        for block in self.tranformer.h:
+            x  = block(x)
+        # forward the final layernorm and the classifier
+        x = self.tranformer.ln_f(x)
+        logits = self.lm_head(x)
+        loss = None
+        if targets is None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), target=(-1))
+        return logits, loss
+    
+    import tiktoken
+    import numpy as np
+
+    def load_tokens(filename):
+        npt = np.load(filename)
+        npt = npt.astype(np.int32)
+        ptt = torch.tensor(npt, dtype=torch.long)
+        return ptt
+    
+    
+        
+
 
